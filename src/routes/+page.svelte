7f0237bcfadfd821c66a595b299479b520142314
Julien Midedji <julien.midedji@gmail.com>
2023-03-14 21:14:41 +0100

    Some first reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+let text = '';
+
+$: {
+    if (text.length > 51) {
+        console.log('full');
+    }
+}
+
+</script>
+
+<div>
+    <textarea bind:value={text} autofocus />
+</div>
+
+<style>
+@font-face {
+    font-family: 'Inter Thin';
+    font-style: normal;
+    font-weight: 100;
+    src: local('Inter Thin'), url('/fonts/Inter.ttf') format('truetype');
+}
+
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+textarea {
+    width: 50%;
+    height: 59px;
+    font-size: 32px;
+
+    font-family: 'Inter Thin';
+
+    border: none;
+    outline: none;
+}
+</style>
