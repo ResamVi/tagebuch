5c645cc18656761fe81e5dfe1abf0ef6b2e594b0
Julien Midedji <julien.midedji@gmail.com>
2023-03-26 16:49:35 +0200

    Introduce proper error handling and send current diary to client.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,60 +1,139 @@
-use log::info;
-use postgres::{Client, NoTls, error::DbError};
+use std::fmt::{Debug, Formatter, Display};
 use std::{thread, time};
 use std::net::{TcpListener, TcpStream};
-use tungstenite::{accept, WebSocket};
-use simple_logger;
+
+use simple_logger::SimpleLogger;
+use log::{info, warn, error, SetLoggerError};
+
+use postgres::{Client, NoTls, error::DbError};
+use tungstenite::{accept, WebSocket, Message};
 
 #[macro_use] 
 use log;
 
-fn main() {
-    simple_logger::SimpleLogger::new().env().init().unwrap();
+const QUERY_SCHEMA: &str = "
+    CREATE TABLE IF NOT EXISTS entries (
+        date        TIMESTAMP PRIMARY KEY DEFAULT CURRENT_TIMESTAMP,
+        content     TEXT NOT NULL
+    )
+";
+
+const QUERY_SELECT: &str = "
+    SELECT content FROM entries WHERE date = date_trunc('day', CURRENT_TIMESTAMP);
+";
+
+/// Declare all errors that can occur when parsing a gitignore file.
+#[derive(Debug)]
+enum Error {
+    /// An error initializing the logger. From reading the lib code this should not occur.
+    LoggerInit(SetLoggerError),
+
+    /// An error in the SQL statement that can't be fixed during runtime.
+    SqlSyntax(postgres::Error),
+
+    /// An error while accepting the TCP connection.
+    TcpConnection(std::io::Error),
+
+    /// An error ...
+    ReadMessage(tungstenite::error::Error),
+}
+
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::LoggerInit(err) => {
+                write!(f, "could not initialize logger: {}", err)
+            },
+            Error::SqlSyntax(err) => {
+                write!(f, "could not define schema: {}", err)
+            },
+            Error::TcpConnection(err) => {
+                write!(f, "could not accept TCP connection: {}", err)
+            },
+            Error::ReadMessage(err) => {
+                write!(f, "could not accept TCP connection: {}", err)
+            },
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    // Initialize logger.
+    match SimpleLogger::new().env().init() {
+        Ok(_) => (),
+        Err(err) => return Err(Error::LoggerInit(err)),
+    }
+    info!("logger initalized");
 
     // Endlessly try to connect.
     let mut client = loop {
         let mut connect_attempt = Client::connect("host=localhost user=postgres password=example", NoTls);
         match connect_attempt {
             Ok(client) => break client,
-            Err(e) => info!("{}", e),
+            Err(e) => warn!("{}", e),
         }
 
         thread::sleep(time::Duration::from_secs(3));
     };
-    println!("connected to database");
+    info!("connected to database");
 
-    // Init database if first time.
-    let schema_defined = client.batch_execute("
-        CREATE TABLE IF NOT EXISTS entries (
-            date        TIMESTAMP PRIMARY KEY DEFAULT CURRENT_TIMESTAMP,
-            content     TEXT NOT NULL
-        )
-    ");
-    if schema_defined.is_err() {
-        panic!("the query was written with an error")
-    }
+    // Init database if it's the first time.
+    match client.batch_execute(QUERY_SCHEMA) {
+        Ok(_) => (),
+        Err(err) => return Err(Error::SqlSyntax(err)),
+    };
 
     // Listen for websocket connections.
-    let server = TcpListener::bind("127.0.0.1:4123").unwrap();
-    for stream in server.incoming() {
-        let mut websocket_conn = accept(stream.unwrap()).unwrap();
+    let server = match TcpListener::bind("127.0.0.1:4123") {
+        Ok(server) => server,
+        Err(err) => todo!(),
+    };
 
-        // 
-        // websocket_conn.write_message()
+    for incoming in server.incoming() {
+        // Accept TCP connection.
+        let stream = match incoming {
+            Ok(stream) => stream,
+            Err(err) => return Err(Error::TcpConnection(err)),
+        };
 
+        // Upgrade to Websocket connection.
+        let mut websocket_conn = match accept(stream) {
+            Ok(websocket_conn) => websocket_conn,
+            Err(err) => {
+                warn!("{}", err); 
+                break;
+            }
+        };
+
+        // Send client what has been written for this day.
+        let row = match client.query_one(QUERY_SELECT, &[]) {
+            Ok(row) => row,
+            Err(err) => panic!("{}", err),
+        };
+        let content: String = row.get("content");
+        websocket_conn.write_message(Message::Text(content));
+
+        // Listen to what client has to write and store it.
         loop {
-            if store(&mut client, &mut websocket_conn) {
-                break
+            match store(&mut client, &mut websocket_conn) {
+                Ok(_) => (),
+                Err(err) => {
+                    error!("{}", err);
+                    break;
+                }
             }
         }
     }
+
+    return Ok(())
 }
 
-fn store(client: &mut Client, connection: &mut WebSocket<TcpStream>) -> bool {
-    // Check if connection closed.
+fn store(client: &mut Client, connection: &mut WebSocket<TcpStream>) -> Result<(), Error> {
+    // Read message (but check if connection closed).
     let content = match connection.read_message() {
         Ok(content) => content,
-        Err(_) => panic!("idk man how to do rust errors"),
+        Err(err) => return Err(Error::ReadMessage(err))
     };
 
     client.execute("
@@ -64,5 +143,5 @@ fn store(client: &mut Client, connection: &mut WebSocket<TcpStream>) -> bool {
        DO UPDATE SET content = $1
     ", &[&content.to_string()]);
 
-    return false;
+    Ok(())
 }
