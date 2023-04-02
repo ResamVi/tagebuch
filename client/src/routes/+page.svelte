261b7547f96f5f5a6bce0fa9ce4e533f5bfa087b
Julien Midedji <julien.midedji@gmail.com>
2023-04-02 10:40:10 +0200

    Decide on a font

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -65,63 +65,10 @@ function keydown(e: KeyboardEvent) {
 
 <style>
 @font-face {
-    font-family: 'Vollkorn';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Vollkorn'), url('/fonts/Vollkorn.ttf') format('truetype');
-}
-
-/* Sahne. Sieht bisschen aus wie Handschrift */
-@font-face {
     font-family: 'Alegreya';
     font-style: normal;
     font-weight: 100;
     src: local('Alegreya'), url('/fonts/Alegreya.ttf') format('truetype');
-}
-
-@font-face {
-    font-family: 'Red Hat Mono Light';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Red Hat Mono Light'), url('/fonts/Red Hat Mono Light.ttf') format('truetype');
-}
-
-@font-face {
-    font-family: 'Cormorant Light';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Cormorant Light'), url('/fonts/Cormorant Light.ttf') format('truetype');
-}
-
-/* Zu breit */
-@font-face {
-    font-family: 'Martel UltraLight';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Martel UltraLight'), url('/fonts/Martel UltraLight.ttf') format('truetype');
-}
-
-
-/* Ehh. Bisschen breit */
-@font-face {
-    font-family: 'Quattrocento';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Quattrocento'), url('/fonts/Quattrocento.ttf') format('truetype');
-}
-
-@font-face {
-    font-family: 'Cardo';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Cardo'), url('/fonts/Cardo.ttf') format('truetype');
-}
-
-@font-face {
-    font-family: 'Inter Thin';
-    font-style: normal;
-    font-weight: 100;
-    src: local('Inter Thin'), url('/fonts/Inter.ttf') format('truetype');
 }
 
 
